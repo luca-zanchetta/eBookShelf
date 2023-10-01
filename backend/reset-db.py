45c0cfac92b9306c666bd1c4ca05b56cd2311b65
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2023-10-01 15:59:33 +0200

    Register API

--- backend/reset-db.py
+++ backend/reset-db.py
@@ -2,3 +2,15 @@ import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['ebookshelf']   # create db 'ebookshelf'
+
+# Reset collections
+collections = db.list_collection_names()
+if len(collections) > 0:
+    for collection in collections:
+        collection.drop()
+
+# Collection 'user'
+user = db['user']
+tmp = {'username':'admin', 'name':'admin', 'surname':'admin', 'balance':100_000, 'password':'ciaociao'}
+x = user.insert_one(tmp)
+print('[INFO] Collection \'user\' created.')
--- backend/server.py
+++ backend/server.py
@@ -20,8 +20,7 @@ def before_request():
 
 # DB setup
 client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client['ebookshelf']   # create db 'ebookshelf'
-
+db = client['ebookshelf']   # create or select db 'ebookshelf'
 
 
 ####################################### REST APIs ###########################################
@@ -33,6 +32,27 @@ def fetch():
 @app.route("/register", methods=['POST'])
 def register():
     data = request.get_json()
+    
+    username = data['username']
+    name = data['name']
+    surname = data['surname']
+    balance = 0
+    password = data['password']
+    
+    user_coll = db['user']
+    new_user = {'username':username, 'name':name, 'surname':surname, 'balance':balance, 'password':password}
+    
+    # Check uniqueness of username
+    for elem in user_coll.find():
+        if elem['username'] == new_user['username']:
+            return jsonify({'message':'ERROR: This username is not valid.', 'status':400})
+    
+    # Insert new user
+    x = user_coll.insert_one(new_user)    
+    if x is not None:
+        return jsonify({'message':f'User {username} has been registered!', 'status':200})
+    
+    return jsonify({'message':'ERROR: User has not been registered.', 'status':500})
 
 
 
