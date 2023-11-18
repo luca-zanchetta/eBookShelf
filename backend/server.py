97b531fc9d39c22515460d4abbf926dabfcbdf52
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2023-11-18 18:37:20 +0100

    Revert "Revert "Fixed search and some scrollbars""
    This reverts commit b3c6191419a9e1b4fdfca0f74b845d67a5d699d3.

--- backend/server.py
+++ backend/server.py
@@ -246,6 +246,7 @@ def get_books():
         return jsonify({'books':[], 'status':201})
 
     for book in book_coll.find():
+        book['_id'] = str(book['_id'])
         books.append(book)
     
     return jsonify({'books':books, 'status':200})
--- frontend/src/Pages/Components/StoreListView.js
+++ frontend/src/Pages/Components/StoreListView.js
@@ -3,36 +3,26 @@ import '../../Css/BookStore-style.css'
 import backIcon from '../../Icons/back.png';
 import sample from '../../Icons/sample.jpg';
 
-const StoreScreens = {
-	Store: 0,
-	ListView: 1,
-}
-
 function StoreListView(props) {
-    var [Status,setStatus] = useState(StoreScreens.ListView);
     const [books, setBooks] = useState(props.books);
     const category = props.currentCategory;
-    const [back, setBack] = useState(true);
 
     function backClick() {
-        setStatus(StoreScreens.Store);
-        setBack(false);
-        setBooks([]);
         window.location.reload(window.location.href);
     }
 
     return(
         <div className="CenterStoreContainer" id='style-1'>
-            {back && 
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <div style={{alignContent:'start'}}>
-                        <input type='image' src={backIcon} alt='back' style={{width:'20%', marginRight: '70%'}} onClick={backClick}></input>
-                    </div>
-                    <div style={{alignContent:'center'}}>
-                        <h1>Category: {category}</h1>
-                    </div>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div style={{alignContent:'start'}}>
+                    <input type='image' src={backIcon} alt='back' style={{width:'20%', marginRight: '70%'}} onClick={backClick}></input>
                 </div>
-            }
+                <div style={{alignContent:'center'}}>
+                    {category &&
+                        <h1>Category: {category}</h1>
+                    }
+                </div>
+            </div>
             {books.length === 0 ? (
                     <h3>No book found!</h3>
                 ) : (
